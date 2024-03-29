d6cc9e8a37bfa42fef023125521abf2039441500
Nicoleta Constantin <constantinnicoleta@hotmail.com>
2024-03-29 00:35:41 +0000

    add validation to check if only letters are entered for the name provided

--- run.py
+++ run.py
@@ -4,7 +4,8 @@ import random
 
 def welcome_screen():
     """
-    Displays welcome message
+    Displays welcome message and prompts for user's name.
+    Try-except block validates data & raises ValueErrors if incorrect type.
     """
     print("Welcome to Let's see if you can guess it!")
     print("The game is simple, you need to guess the random word letter by letter...")
@@ -12,7 +13,21 @@ def welcome_screen():
     print("I'm sure you can guess it before that...Let's play!")
     print("But first...")
     
-    
+    while True:
+        try:
+            name = input("Please enter your name: ")
+            if not name.strip():
+                raise ValueError("Name cannot be left blank.")
+            elif not name.isalpha():
+                raise ValueError("Please only enter letters for your name.")
+            else:
+                print(f'Hi {name}, now we are ready to play!')
+                break
+        except ValueError as e:
+            print(f"Invalid input: {e}")
+        
 welcome_screen()
+
+
   
     
