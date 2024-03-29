296edc5fc0f40e1e579be70a166a287c7561dadd
Nicoleta Constantin <constantinnicoleta@hotmail.com>
2024-03-29 00:29:23 +0000

    create function to display welcome message and how to play

--- run.py
+++ run.py
@@ -1,3 +1,18 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# library used to choose a word 
+#from a random list of words 
+import random 
+
+def welcome_screen():
+    """
+    Displays welcome message
+    """
+    print("Welcome to Let's see if you can guess it!")
+    print("The game is simple, you need to guess the random word letter by letter...")
+    print("Did I mention you're given only have 6 attempts?")
+    print("I'm sure you can guess it before that...Let's play!")
+    print("But first...")
+    
+    
+welcome_screen()
+  
+    
